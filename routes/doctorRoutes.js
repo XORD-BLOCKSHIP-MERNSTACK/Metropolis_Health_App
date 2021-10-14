1fd4f628dd076faf6cd576496c72dc45e43864e4
Ayesha Ghani <60398800+ayesha-ghani098@users.noreply.github.com>
2021-10-15 02:54:41 +0500

    added treatment routes

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,14 +1,21 @@
-import express from "express";
+import express from 'express';
 
 // Controllers
-import { createEntry, entryListing } from "../controllers/doctor.js";
+import {
+  createEntry,
+  entryListing,
+  createTreatment,
+  treatmentListing,
+} from '../controllers/doctor.js';
 
-import protect from "../middleware/auth.js";
+import protect from '../middleware/auth.js';
 
 const router = express.Router();
 
 // Routes
-router.route("/createEntry").post(protect, createEntry);
-router.route("/entryListing").get(protect, entryListing);
+router.route('/createEntry').post(protect, createEntry);
+router.route('/entryListing').get(protect, entryListing);
+router.route('/createTreatment').post(protect, createTreatment);
+router.route('/treatments').get(protect, treatmentListing);
 
 export default router;
